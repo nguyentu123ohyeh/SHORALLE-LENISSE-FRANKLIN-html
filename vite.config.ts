09109e94393d9c0ab75c7fe174be698e474df2c4
minhtv901 <le5875970@gmail.com>
2026-05-18 16:10:37 +0700

    sada

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  base: '/SHORALLE-LENISSE-FRANKLIN-html/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
